34418c519d4b13b8a55b224cc3dfbafa4a12a8bf
Sekator778 <sekator778@gmail.com>
2020-03-16 14:24:46 +0200

    TestGenerator

--- chapter_001/src/test/java/TestGenerator.java
+++ chapter_001/src/test/java/TestGenerator.java
@@ -9,70 +9,53 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class TestGenerator {
-    Template template = new GeneratorString();
-
     @Test
     public void whenHaveOneKey() {
+        Template template = new GeneratorString();
         String string = "I love ${subject}";
-        Map<String, String> map = new HashMap<>() {
-            {
-                put("subject", "Java");
-            }
-        };
+        Map<String, String> map = new HashMap<>();
+        map.put("subject", "Java");
         String result = template.generate(string, map);
         assertThat(result, is("I love Java"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenNotHaveKeyThrowException() {
+        Template template = new GeneratorString();
         String string = "I love ${key}";
-        Map<String, String> map = new HashMap<>() {
-            {
-                put(null, "Java");
-            }
-        };
+        Map<String, String> map = new HashMap<>();
         String result = template.generate(string, map);
         assertThat(result, is("I love Java"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenNotHaveKey() {
+        Template template = new GeneratorString();
         String string = "I love ${one}";
-        Map<String, String> map = new HashMap<>() {
-            {
-                put("key", "Java");
-            }
-        };
+        Map<String, String> map = new HashMap<>();
         String result = template.generate(string, map);
         assertThat(result, is("I love Java"));
     }
 
     @Test
     public void whenHaveManykeys() {
+        Template template = new GeneratorString();
         String string = "I love ${key} and i wont ${do} very ${how}";
-        Map<String, String> map = new HashMap<>() {
-            {
-                put("key", "Java");
-                put("do", "study");
-                put("how", "hard");
-            }
-        };
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "Java");
+        map.put("do", "study");
+        map.put("how", "hard");
         String result = template.generate(string, map);
         assertThat(result, is("I love Java and i wont study very hard"));
     }
 
     @Test
     public void whenHaveManySimilarKey() {
+        Template template = new GeneratorString();
         String string = "Help, ${sos}, ${sos}, ${sos}";
-        Map<String, String> map = new HashMap<>() {
-            {
-                put("sos", "Aaa");
-                put("do", "study");
-                put("how", "hard");
-            }
-        };
+        Map<String, String> map = new HashMap<>();
+        map.put("sos", "Aaa");
         String result = template.generate(string, map);
         assertThat(result, is("Help, Aaa, Aaa, Aaa"));
     }
-
 }
