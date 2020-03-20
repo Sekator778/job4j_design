978f2aa727d2fda4692aab61d4f9e6dff14e4017
Sekator778 <sekator778@gmail.com>
2020-03-20 15:55:50 +0200

    1. Читаем файл конфигурации[#252826] тест

--- chapter_002/src/test/java/io/TestAnalyze.java
+++ chapter_002/src/test/java/io/TestAnalyze.java
@@ -4,18 +4,25 @@ import config.Config;
 import org.junit.Test;
 import server.Analyze;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TestAnalyze {
 
     @Test
-    public void whenPairWithoutComment() {
+    public void whenPairWithoutComment() throws IOException {
         String path = "/home/sekator/projects/job4j_design/server.log";
         new Analyze().unavailable("/home/sekator/projects/job4j_design/server.log", "target.txt");
-//        assertThat(
-//                config.value("login"),
-//                is("Admin")
-//        );
+        BufferedReader fileReader = new BufferedReader(new FileReader("target.txt"));
+        String one = fileReader.readLine();
+        assertThat(
+                one,
+                is("10:57:01")
+        );
     }
 }
