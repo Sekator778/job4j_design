303a33d8495e5f41040eeaf3019ec056e77bb96a
Sekator778 <sekator778@gmail.com>
2020-03-26 10:26:39 +0200

    архиватор определенных файлов

--- chapter_002/src/main/java/io/archive/Zip.java
+++ chapter_002/src/main/java/io/archive/Zip.java
@@ -6,34 +6,65 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * Задана директория проекта, например: c:\project\job4j\
+ * 2. В качестве ключа передается  расширения файлов, которые не нужно включать в архив.
+ * 3. Архив должен сохранять структуру проекта.
+ * 4. Запуск проекта java -jar pack.jar -d c:\project\job4j\ -e *.java -o project.zip
+ * <p>
+ * java -jar pack.jar - Это собранный jar.
+ * <p>
+ * -d - directory - которую мы хотим архивировать
+ * -e - exclude - исключить файлы *.xml
+ * -o - output - во что мы архивируем.
+ */
 public class Zip {
-    private List<File> seekBy(String root, String ext) throws IOException {
+    /**
+     * https://mkyong.com/java/java-files-walk-examples/
+     * walk  - это гулять вот он нам и по нашему пути делает стрим какой
+     * нам надо
+     *
+     * @param root где гулять директория
+     * @param ext  - чего исключать по строке в имени
+     * @return - лист файлов которые проходят
+     */
+    private List<File> seekBy(String root, String ext) {
         List<File> list = new ArrayList<>();
-        Files.walk(Paths.get(root))
-                .filter(Files::isRegularFile)
-                .filter(path -> !path.getFileName().toString().endsWith(ext))
-                .forEach(i -> list.add(i.toFile()));
+        try (Stream<Path> walk = Files.walk(Paths.get(root))) {
+            walk
+                    .filter(Files::isRegularFile)
+                    .filter(path -> !path.getFileName().toString().endsWith(ext))
+                    .forEach(i -> list.add(i.toFile()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return list;
     }
 
-    public static void pack(File source, String targetS) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(targetS)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
+    /**
+     * @param source - лист файлов для подальшей архивации
+     * @param output - path zip output file
+     */
+    public static void pack(List<File> source, String output) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(output)))) {
+            for (File file : source
+            ) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Args args1 = new Args(args);
-        new Zip().seekBy(args1.directory(), args1.excuse())
-                .forEach(file ->
-                        pack(file, args1.output()));
-     }
+        pack(new Zip().seekBy(args1.directory(), args1.excuse()), args1.output());
+    }
 }
