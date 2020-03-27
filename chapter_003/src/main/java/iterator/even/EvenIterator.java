c72463c57dfe897328a1d45a7d4d135705332ac5
Sekator778 <sekator778@gmail.com>
2020-03-27 15:40:39 +0200

    even iterator

--- chapter_003/src/main/java/iterator/even/EvenIterator.java
+++ chapter_003/src/main/java/iterator/even/EvenIterator.java
@@ -3,6 +3,15 @@ package iterator.even;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * this cod find even number for array
+ * example new int[]{1, 2, 3, 4, 5, 6, 7}
+ * must find 2, 4
+ *
+ * method hasNext gives true only when even
+ * example = 1, -> hasNext -> 3, 4,  -true
+ * example = 1, -> hasNext -> 3, 5,  -false
+ */
 public class EvenIterator implements Iterator<Integer> {
     private final int[] values;
     private int index;
@@ -18,32 +27,33 @@ public class EvenIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-        int rsl = 0;
-        if (evenNext()) {
-            while (index < values.length) {
-                if (even(index)) {
-                    rsl = values[index];
-                    index++;
-                    break;
-                }
-                index++;
-            }
+         if (!evenNext()) {
+            throw  new NoSuchElementException();
         } else {
-            throw new NoSuchElementException();
+            return values[index++];
         }
-
-        return rsl;
     }
 
+    /**
+     * check number even or no
+     * @param x - number for check
+     * @return - true if even number
+     */
     public boolean even(int x) {
         return ((this.values[x] % 2) == 0);
     }
+
+    /**
+     * method find even to the end of the array
+     * @return - are the or not even element in the array
+     */
 
     public boolean evenNext() {
         boolean rsl = false;
         for (int i = index; i < values.length; i++) {
             if (even(i)) {
                 rsl = true;
+                this.index = i;
                 break;
             }
         }
