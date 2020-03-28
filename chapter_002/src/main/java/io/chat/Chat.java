486b373294bc69e293470f1f6295d6d1d20f1fc0
Sekator778 <sekator778@gmail.com>
2020-03-28 09:48:36 +0200

    5.2.1. Реализовать SimpleArray<T>[#252796]

--- chapter_002/src/main/java/io/chat/Chat.java
+++ chapter_002/src/main/java/io/chat/Chat.java
@@ -1,29 +1,34 @@
 package io.chat;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Chat {
     public static void main(String[] args) {
+        final String STOP = "stop";
+        final String GO = "go";
         String start = "Start chat \n stop - for stop chat;\n go - for continuation chat";
-        String question;
-        String answer;
         boolean stop = true;
+
         Scanner scanner = new Scanner(System.in);
         try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(
                 new FileOutputStream("chatLog.txt")))) {
             System.out.println(start);
             writer.write(start + "\n");
-            question = scanner.nextLine();
+            String question = scanner.nextLine();
+
             while (!(question.equalsIgnoreCase("exit"))) {
                 writer.write("Q:" + question + "\n");
-                if (question.equals("stop")) {
+                if (STOP.equals(question)) {
                     stop = false;
-                } else if (question.equals("go")) {
+                } else if (GO.equals(question)) {
                     stop = true;
                 }
                 if ((stop) && !question.isEmpty()) {
-                    answer = Answer.chatAnswer();
+                    String answer = Answer.chatAnswer();
                     System.out.println(answer);
                     writer.write("A:" + answer + "\n");
                 }
