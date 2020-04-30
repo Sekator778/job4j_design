c6dc1d0fac528869037faeaeeec3647a45d9b1c9
Sekator778 <sekator778@gmail.com>
2020-04-30 17:38:07 +0300

    SQL Select by company[#252766]

---
+++ chapter_004/src/main/java/sql/SQL Select by company[#252766].sql
@@ -0,0 +1,49 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer,
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+insert into company (id, name)
+values
+(1, 'Bosh'),
+       (2, 'Sony'),
+       (3, 'Oracle'),
+       (4, 'Sun'),
+       (5, 'Yahoo');
+
+select * from company;
+
+insert into person (id, name, company_id) VALUES
+(1, 'Bob', 1),
+(2, 'Pol', 2),
+(3, 'Molli', 3),
+(4, 'Scala', 4),
+(5, 'Java', 5),
+(6, 'Rube', 1),
+(7, 'PHP', 1);
+select * from person;
+
+--// 1) Получить в одном запросе:
+-- // - имена всех лиц, которые НЕ находятся в компании с id = 5
+-- // - название компании для каждого человека
+select distinct person.name, c.name from person left join company c on person.company_id = c.id
+where c.id = 5
+order by person.name;
+select distinct person.name, c.name from person left join company c on person.company_id = c.id
+where c.id <> 5
+order by person.name;
+-- // 2) Выберите название компании с максимальным количеством людей + количество людей в этой компании
+select c.name, count(p.company_id) from company c left join person p on c.id = p.company_id
+group by c.name
+order by count(p.company_id) desc
+limit 1;
+
--- chapter_004/src/main/java/sql/test.sql
+++
@@ -1,6 +0,0 @@
-create table test_my(
-    id serial primary key,
-    name varchar(90)
-);
-select * from test_my;
-insert into test_my values (name)
