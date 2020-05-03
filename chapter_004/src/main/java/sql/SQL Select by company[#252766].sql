ea59b0e0a03744613ff3030011995cfd31c902fb
Sekator778 <sekator778@gmail.com>
2020-05-03 08:51:05 +0300

    SQL Select by company[#252766]

--- chapter_004/src/main/java/sql/SQL Select by company[#252766].sql
+++ chapter_004/src/main/java/sql/SQL Select by company[#252766].sql
@@ -23,29 +23,27 @@ values
 select * from company;
 
 insert into person (id, name, company_id) VALUES
-(1, 'Bob', 1),
-(2, 'Pol', 2),
-(3, 'Molli', 3),
-(4, 'Scala', 4),
-(5, 'Java', 5),
-(6, 'Rube', 1),
-(7, 'PHP', 1);
+(1, 'Kolya', 1),
+(2, 'Vasya', 2),
+(3, 'Olya', 3),
+(4, 'Misha', 4),
+(5, 'Oleg', 5),
+(6, 'Sasha', 1),
+(7, 'Vasiliy', 1);
 select * from person;
 
 --// 1) Получить в одном запросе:
 -- // - имена всех лиц, которые НЕ находятся в компании с id = 5
 -- // - название компании для каждого человека
 -- do it..........
+truncate table person restart identity ;
 
 
-
-
-select distinct person.name, c.name from person left join company c on person.company_id = c.id
-where c.id = 5
-order by person.name;
 select distinct person.name, c.name from person left join company c on person.company_id = c.id
 where c.id <> 5
 order by person.name;
+--
+select person.name from person where company_id <> 5;
 -- // 2) Выберите название компании с максимальным количеством людей + количество людей в этой компании
 -- do it .......
 
