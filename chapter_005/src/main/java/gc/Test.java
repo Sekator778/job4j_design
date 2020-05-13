ed375c424325d74e44c53addd9ff7a83c4a9c1bd
Sekator778 <sekator778@gmail.com>
2020-05-13 19:06:50 +0300

    Реализации кеша на SoftReference[#252875]

---
+++ chapter_005/src/main/java/gc/Test.java
@@ -0,0 +1,12 @@
+package gc;
+
+/**
+ *
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "name.txt";
+
+    }
+}
--- chapter_005/src/test/java/gc/CacheTest.java
+++ chapter_005/src/test/java/gc/CacheTest.java
@@ -13,12 +13,35 @@ import static org.junit.Assert.assertThat;
 public class CacheTest {
 
     @Test
+    public void whenReadSimpleNameTXT() {
+        String expected = "Nick" + System.lineSeparator() + "Masha" + System.lineSeparator() + "Victor";
+        String name = "name.txt";
+        File file = new File("/home/sekator/projects/job4j_design/chapter_005/src/main/resources/name.txt");
+        String absolutePath = file.getAbsolutePath();
+        String tempFilePath = absolutePath.substring(0, absolutePath.lastIndexOf(File.separator));
+        Cache cacheFileData = new Cache(tempFilePath);
+        assertThat(cacheFileData.getValue(name), is(expected));
+    }
+
+    @Test
+    public void whenReadSimpleAddressTXT() {
+        String expected = "Dom" + System.lineSeparator() + "Pyshkina" + System.lineSeparator() + "Ylitsa";
+        String name = "address.txt";
+        File file = new File("/home/sekator/projects/job4j_design/chapter_005/src/main/resources/address.txt");
+        String absolutePath = file.getAbsolutePath();
+        String tempFilePath = absolutePath.substring(0, absolutePath.lastIndexOf(File.separator));
+        Cache cacheFileData = new Cache(tempFilePath);
+        assertThat(cacheFileData.getValue(name), is(expected));
+    }
+
+    @Test
     public void whenReadOneWhenLoadFromFile() throws IOException {
         // строка ожидаемая с разделителями
         String expected = "Alex" + System.lineSeparator() + "Kolya" + System.lineSeparator() + "Dilon";
         // делаем тмп файл
         File file = File.createTempFile("temp", ".txt");
         //записываем в него строку ожидаемую
+//        System.out.println(" temp file "  + file);
         Files.write(file.toPath(), expected.getBytes());
         // берем имя файла тмп
         String name = file.getName();
@@ -30,7 +53,6 @@ public class CacheTest {
         Cache cacheFileData = new Cache(tempFilePath);
         // читаем все что в нем есть и сравниваем со строкой начальной
         assertThat(cacheFileData.getValue(name), is(expected));
-
     }
 
     @Test
@@ -48,4 +70,5 @@ public class CacheTest {
         assertThat(cacheFileData.getValue(name), is(expected));
         assertThat(Files.readString(file.toPath()), is("JAVA"));
     }
+
 }
