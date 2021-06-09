d4de8d979a87c03c0a0557ac538a15595392a7b4
Sekator778 <sekator778@gmail.com>
2021-06-09 22:58:50 +0300

    likes facebook

---
+++ tricks/src/main/java/meta/Solution.java
@@ -0,0 +1,18 @@
+package meta;
+
+class Solution {
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0:
+                return "no one likes this";
+            case 1:
+                return String.format("%s likes this", names[0]);
+            case 2:
+                return String.format("%s and %s like this", names[0], names[1]);
+            case 3:
+                return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default:
+                return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
+    }
+}
---
+++ tricks/src/test/java/meta/SolutionTest.java
@@ -0,0 +1,16 @@
+package meta;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", Solution.whoLikesIt());
+        assertEquals("Peter likes this", Solution.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", Solution.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", Solution.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", Solution.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+}
